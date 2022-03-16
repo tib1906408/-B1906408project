18eaf381b6c02ed7dd90c3e361d087e51465d2f2
tib1906408 <101680894+tib1906408@users.noreply.github.com>
2022-03-16 09:25:37 +0700

    Add files via upload

---
+++ sua_mk.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+
+<form action="log.php" method="post">
+Password: <input type="password" name="pass"><br>
+
+
+<label for="newpass">Mật khẩu mới</label>
+    <input type="password" name="newpass" id="newpass" />
+    
+    <br />
+
+    <label for="renewpass">Lặp lại mật khẩu mới</label>
+    <input type="password" name="renewpass" id="renewpass" />
+	
+
+
+<input type="submit">
+
+</form>
+
+</body>
+</html>
